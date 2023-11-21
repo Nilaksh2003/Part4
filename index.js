189498ca66e8b412a1521d36c15cfa52722ce078
Nilaksh2003 <nilaksh.ginilytics@gmail.com>
2023-11-21 18:25:46 +0530

    Blog list, step2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const app=require('./app') // the actual Express application
+const config=require('./utils/config')
+const logger= require('./utils/logger')
+
+app.listen(config.PORT,()=>{
+    logger.info(`Server running on port ${config.PORT}`)
+})
