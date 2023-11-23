26d8ec6d2184c11c5a4ba634e27a2f4fb3fd2b67
Nilaksh2003 <nilaksh.ginilytics@gmail.com>
2023-11-23 10:56:29 +0530

    helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,31 @@ const totalLikes =(blogs)=>{
     return count
 }
 
+const favoriteBlog =(blogs)=>{
+    if(blogs.length===0)
+    {
+        return null
+    }
+    let maxLikes=blogs[0].likes
+    let maxLikesBlog=blogs[0]
+    for(let i=1; i<blogs.length;i++)
+    {
+        if(blogs[i].likes>maxLikes)
+        {
+            maxLikes=blogs[i].likes
+            maxLikesBlog=blogs[i]
+        }
+    }
+    let favBlog={
+        title: maxLikesBlog.title,
+        author: maxLikesBlog.author,
+        likes: maxLikesBlog.likes
+    }
+    return favBlog
+}
+
 module.exports={
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
